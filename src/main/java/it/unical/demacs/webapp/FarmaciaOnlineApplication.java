b3e8940d53ea1bcfd3f301b06e7d1c22b04c9b96
Scangwski <108692473+Scangwski@users.noreply.github.com>
2024-01-10 17:02:34 +0100

    Initial commit

---
+++ src/main/java/it/unical/demacs/webapp/FarmaciaOnlineApplication.java
@@ -0,0 +1,13 @@
+package it.unical.demacs.webapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FarmaciaOnlineApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FarmaciaOnlineApplication.class, args);
+	}
+
+}
---
+++ src/test/java/it/unical/demacs/webapp/FarmaciaOnlineApplicationTests.java
@@ -0,0 +1,13 @@
+package it.unical.demacs.webapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FarmaciaOnlineApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
