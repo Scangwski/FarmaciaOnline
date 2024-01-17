b51897441f983d7919caecd4eed9e762a60aec35
Carlo Ianni <nnicrl99a07h579n@studenti.unical.it>
2024-01-17 06:25:27 +0100

    aggiunta di angular a varie componenti

---
+++ src/app/recupera-password/recupera-password.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-recupera-password',
+  templateUrl: './recupera-password.component.html',
+  styleUrl: './recupera-password.component.css'
+})
+export class RecuperaPasswordComponent {
+
+}
