c5c07755a1c9151f6b91f33b155fd8a9b2aaf9ab
Scangwski <108692473+Scangwski@users.noreply.github.com>
2024-01-19 20:31:04 +0100

    aggiornate le rotte

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,14 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import {LoginComponent} from "./login/login.component";
+import { HomeComponent } from './home/home.component'; // Importa HomeComponent
+import { CarrelloComponent } from './carrello/carrello.component'; // Importa CarrelloComponent
 
 const routes: Routes = [
-  {"path" : "login", component:LoginComponent}
-];
+  { path: '', redirectTo: '/home', pathMatch: 'full' }, // Reindirizza il path vuoto alla home
+    { path: 'home', component: HomeComponent },
+    { path: 'carrello', component: CarrelloComponent },
+    // ... altre rotte
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
