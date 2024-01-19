00c618cb40f67feba068f6ec2f218101c52bfb0a
Carlo Ianni <nnicrl99a07h579n@studenti.unical.it>
2024-01-20 00:24:52 +0100

    -

---
+++ src/app/pagina-prodotto/pagina-prodotto.ts
@@ -0,0 +1,8 @@
+import { Component } from '@angular/core';
+
+@Component({
+    selector: 'app-pagina-prodotto',
+    templateUrl: './pagina-prodotto.component.html',
+    styleUrls: ['./pagina-prodotto.component.css']
+})
+export class PaginaProdotto {}
