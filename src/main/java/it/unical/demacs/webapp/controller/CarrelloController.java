056ebaa8fe88ea292f7809b9c9bc86b57efa5ff6
lilballsmike <107635479+lilballsmike@users.noreply.github.com>
2024-01-24 00:37:29 +0100

    controller carrello

--- src/main/java/it/unical/demacs/webapp/controller/CarrelloController.java
+++ src/main/java/it/unical/demacs/webapp/controller/CarrelloController.java
@@ -1,61 +1,39 @@
 package it.unical.demacs.webapp.controller;
 
-
 import it.unical.demacs.webapp.model.Carrello;
-import it.unical.demacs.webapp.model.Ricetta;
+import it.unical.demacs.webapp.model.Prodotto;
 import it.unical.demacs.webapp.model.Utente;
 import it.unical.demacs.webapp.persistance.jdbc.DatabaseJDBC;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
-
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
-import javax.xml.crypto.Data;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+
 import java.sql.SQLException;
 import java.util.ArrayList;
 
-/*
-@RestController
-public class CarrelloController
-{
-    @PostMapping("/aggiungiNelCarrello")
-    public void aggiungiNelCarrello(HttpServletResponse res,HttpServletRequest req, @RequestBody String nomeProdotto) throws SQLException
-    {
-        Utente u = (Utente) req.getSession().getAttribute("utente");
-        if(DatabaseJDBC.getInstance().getCarrelloDao().inserisciNelCarrello(u.getEmail(),nomeProdotto.replaceAll("\"","")))
-            res.setStatus(HttpServletResponse.SC_OK);
-        else
-            res.setStatus(HttpServletResponse.SC_SERVICE_UNAVAILABLE);
-    }
+import static jakarta.servlet.http.HttpServletResponse.SC_OK;
+import static jakarta.servlet.http.HttpServletResponse.SC_SERVICE_UNAVAILABLE;
+
+public class CarrelloController {
+
 
     @PostMapping("/caricaCarrello")
-    public ArrayList<Carrello> caricaCarrello(HttpServletRequest req, HttpServletResponse res) throws SQLException
+    public ArrayList<Carrello> caricaCarrello(HttpServletRequest req,HttpServletResponse res) throws SQLException
     {
         Utente u=(Utente)req.getSession().getAttribute("utente");
-        ArrayList<Carrello> contenuto=DatabaseJDBC.getInstance().getCarrelloDao().prelevaCarrello(u.getEmail());
+        ArrayList<Carrello> contenuto=DatabaseJDBC.getInstance().getCarrelloDao().prelevaCarrello(u);
 
-        if(contenuto.isEmpty())
-            res.setStatus(HttpServletResponse.SC_SERVICE_UNAVAILABLE);
-        else
-            res.setStatus(HttpServletResponse.SC_OK);
 
         return contenuto;
     }
 
-    @PostMapping("/svuotaCarrello")
-    public void svuotaCarrello(HttpServletRequest req) throws SQLException
-    {
-        Utente u=(Utente)req.getSession().getAttribute("utente");
-        DatabaseJDBC.getInstance().getCarrelloDao().svuotaCarrello(u.getEmail());
-    }
 
     @PostMapping("/eliminaArticolo")
-    public void eliminaArticolo(HttpServletRequest req,@RequestBody String nomeProdotto) throws SQLException
+    public void eliminaArticolo(HttpServletRequest req,@RequestBody Prodotto prodotto) throws SQLException
     {
-        nomeProdotto=nomeProdotto.replaceAll("\"","");
         Utente u=(Utente)req.getSession().getAttribute("utente");
-        DatabaseJDBC.getInstance().getCarrelloDao().eliminaArticolo(nomeProdotto,u.getEmail());
+        DatabaseJDBC.getInstance().getCarrelloDao().eliminaArticolo(u,prodotto);
     }
-}*/
+
+}
