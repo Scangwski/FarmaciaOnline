1c054c1886809077d4f65da172bac8e249bbb554
Carlo Ianni <carloianni71@gmail.com>
2024-01-27 17:53:25 +0100

    inventario.html ora ha un campo per il prezzo, aggiunto inventario.js (da sistemare e finire)

---
+++ src/main/resources/static/js/inventario.js
@@ -0,0 +1,50 @@
+function creaProdotto() {
+
+    var idProdotto = document.getElementById('idProdotto').value;
+
+    var nomeProdotto = document.getElementById('nomeProdotto').value;
+
+    var descrizioneProdotto = document.getElementById('descrizioneProdotto').value;
+
+    var prezzoProdottoString = document.getElementById('prezzoProdotto').value;
+    var prezzoProdotto = parseFloat(prezzoProdottoString.replace(',', '.'));
+    if (isNaN(prezzoProdotto)) {
+        alert("prezzo inserito non valido!");
+    }
+
+    var categoriaProdotto = document.getElementById('categoriaProdotto').value;
+
+    var quantitaProdotto = document.getElementById('counterValue2').value;
+
+    var immagineProdotto = document.getElementById('uploadImmagine').value;
+
+
+
+    alert("aggiunti: " + idProdotto+","+ nomeProdotto+","+ descrizioneProdotto+","+ prezzoProdotto+","+ categoriaProdotto+","+ quantitaProdotto+","+ immagineProdotto);
+    //aggiuntaProdotto(idProdotto, nomeProdotto, descrizioneProdotto, prezzoProdotto, categoriaProdotto, quantitaProdotto, immagineProdotto)
+}
+
+
+function aggiuntaProdotto(id, nome, descrizione, categoria, immagine) {
+
+    fetch('/aggiuntaProdotto', {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({
+            id: id,
+            nome: nome,
+            descrizione: descrizione,
+            categoria: categoria,
+            immagine: immagine
+        }),
+    })
+        .then(response => response.json())
+        .then(data => {
+            console.log('Risposta dal controller:', data);
+        })
+        .catch(error => {
+            console.error('Errore durante la chiamata al controller:', error);
+        });
+}
