4960aa78579cfc3eb38e319b9e481fffa6c87473
lilballsmike <107635479+lilballsmike@users.noreply.github.com>
2024-01-28 19:58:14 +0100

    carrello ultimato

--- src/main/java/it/unical/demacs/webapp/persistance/CarrelloDao.java
+++ src/main/java/it/unical/demacs/webapp/persistance/CarrelloDao.java
@@ -1,7 +1,6 @@
 package it.unical.demacs.webapp.persistance;
 
 import it.unical.demacs.webapp.model.Carrello;
-import it.unical.demacs.webapp.model.Prodotto;
 import it.unical.demacs.webapp.model.Utente;
 
 import java.sql.SQLException;
@@ -11,6 +10,8 @@ public interface CarrelloDao {
     public boolean inserisciNelCarrello(String emailutente, String nomeProdotto) throws SQLException;
     public ArrayList<Carrello> prelevaCarrello(Utente utente)throws  SQLException;
     public void eliminaArticolo(String email, String prodotto) throws  SQLException;
+
+    public boolean aggiornaPrezzo(String email, String nomeprodotto, Double prezzo) throws SQLException;
 
 
 }
