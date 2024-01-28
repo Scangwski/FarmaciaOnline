f1041d2c58d625fb2ba595d71aec89175b560972
Scangwski <108692473+Scangwski@users.noreply.github.com>
2024-01-28 23:15:17 +0100

    fix finito

--- src/main/resources/static/js/paginaprodotto.js
+++ src/main/resources/static/js/paginaprodotto.js
@@ -15,10 +15,6 @@ document.getElementById("homeButton").addEventListener("click", function (){
     window.location.href = '/home';
 });
 
-document.getElementById("profilobtn").addEventListener("click", function (){
-    window.location.href = '/paginautente';
-});
-
 document.getElementById("carr").addEventListener("click",function (){
     window.location.href='/carrello';
 })
@@ -37,6 +33,12 @@ function caricaProdotto(){
     var nome= document.getElementById("nome").innerText=prodottoSelezionato.nome;
     var descrizione=document.getElementById("descrizione").innerText=prodottoSelezionato.descrizione;
     var prezzo= document.getElementById("prezzo").innerText=prodottoSelezionato.prezzo +"€";
+    var urlImmagine="oooooooooooooooooaa"
+    var imgElement = document.getElementById("img");
+    var nuovoUrl = prodottoSelezionato.immagine;
+    imgElement.setAttribute("src", nuovoUrl);
+
+
 }
 function caricaRecensioni(nomeProdotto) {
     fetch('/recensioni?nomeprodotto=' + encodeURIComponent(nomeProdotto))
@@ -87,6 +89,8 @@ function aggiungiRecensioneAlDOM(nomeUtente, testoRecensione, valutazione) {
 // Assumi che 'prodottoSelezionato' sia un oggetto che contiene le informazioni del prodotto, incluso il nome.
 caricaRecensioni(prodottoSelezionato.nome);
 
+
+
 function inserisciRicetta() {
     var ricettaValue = document.querySelector("#ricetta").value;
 
@@ -107,12 +111,15 @@ function inserisciRicetta() {
                 var sconto= document.getElementById("sconto");
                 prezzo.innerText="0€"
                 sconto.innerText="Risparmio del 100% (Ricetta)"
+                prodottoSelezionato.prezzo=0;
+                localStorage.setItem('prodottoSelezionato', JSON.stringify(prodottoSelezionato));
                 alert("Ricetta inserita con successo!");
             },
             error: function (xhr, status, error) {
                 // Gestisci gli errori
                 alert("Codice non valido o gia usato!");
             },
+
         });
 }
 
@@ -154,7 +161,7 @@ function inviaRecensione() {
     });
 }
 
-function aggiungiNelCarrello(nomeProdotto)
+function aggiungiNelCarrello()
 {
     console.log();
     $.ajax(
@@ -165,7 +172,40 @@ function aggiungiNelCarrello(nomeProdotto)
             data: JSON.stringify(prodottoSelezionato.nome),
             success: function()
             {
-                alert("Prodotto correttamente aggiunto nel carrello!");
+                if(prodottoSelezionato.prezzo==0){
+                    aggiornaPrezzo();
+                    alert("Prodotto azzerato correttamente aggiunto nel carrello!");
+
+                }
+                else{
+                    alert("Prodotto correttamente aggiunto nel carrello!");
+                }
+
+            }
+        })
+}
+function Carrello(prodotti,emailUtente,quantita, prezzoTotale) {
+    this.prodotti = prodotti;
+    this.emailUtente=emailUtente;
+    this.quantita=quantita;
+    this.prezzoTotale = prezzoTotale;
+}
+
+function aggiornaPrezzo(){
+    var nome= prodottoSelezionato.nome;
+    var prezzo= prodottoSelezionato.prezzo;
+    var carrello= new Carrello(nome,null,1,prezzo);
+    console.log(carrello)
+
+    $.ajax(
+        {
+            type:"POST",
+            url:"/aggiornaPrezzo",
+            contentType: "application/json",
+            data: JSON.stringify(carrello),
+            success: function()
+            {
+
             }
         })
 }
